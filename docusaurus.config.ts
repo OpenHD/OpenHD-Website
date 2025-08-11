61a6a3f1fe250f1fbc220d11486e259b78ef1327
Limitless Green <LimitlessGreen@outlook.de>
2025-08-11 19:11:02 +0200

    Fix edit link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -208,7 +208,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           routeBasePath: '/',
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/OpenHD/OpenHD-Website/edit/main/docusaurus-website/',
+          editUrl: 'https://github.com/OpenHD/OpenHD-Website/edit/main/',
           // Versioning configuration
           lastVersion: 'current',
           includeCurrentVersion: true,
